31bdf8560290314bcdb87a186e695dd457ffcee6
Joiter89 <123334834+Joiter89@users.noreply.github.com>
2023-05-06 03:56:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ from book_sales import get_book_sales
 DSN = 'postgresql://postgres:12345678@localhost:5432/books_db'
 engine = sqlalchemy.create_engine(DSN)
 
-# create_tables(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# insert_data(session)
 
 if __name__ == '__main__':
     create_tables(engine)
